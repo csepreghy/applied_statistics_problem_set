94da710c06ca0b6fd749762909598b878fc3f378
Andrew <acsepreghy@ganintegrity.com>
2020-01-03 20:04:22 +0100

    simulated gaussian variables for v and T

--- error_propagation.py
+++ error_propagation.py
@@ -12,9 +12,9 @@ import math
 
 from distributions_probability import func_gaussian_pdf
 from plotify import Plotify
-from ExternalFunctions import nice_string_output, add_text_to_ax, Chi2Regression, integrate1d
 
 plotify = Plotify()
+r = np.random
 
 def chi2(values, uncertainties, n_parameters):
   mean = np.mean(values)
@@ -32,12 +32,22 @@ def chi2(values, uncertainties, n_parameters):
 
   return chi2_value, p_chi2
 
-def exercise_2_1():    
+def compute_B(v, T):
+  h = 6.626e-34
+  c = 299.7e6
+  kB = 1.381e-23
+
+  first_term = (2 * h * v ** 3) / c ** 2
+  second_term = 1 / (math.exp( (h * v) / (kB * T) ) - 1)
+
+  return first_term * second_term
+
+def exercise_2_1():
   n_parameters_A = 1
   values_A = np.array([2.05, 2.61, 2.46, 2.48])
   uncertainties_A = np.array([0.11, 0.10, 0.13, 0.12])
 
-  chi2_value_A, p_chi2_A = chi2(values_A, uncertainties_A, n_parameters_A) 
+  chi2_value_A, p_chi2_A = chi2(values_A, uncertainties_A, n_parameters_A)
 
   print(f'chi2_value_A = {chi2_value_A}')
   print(f'p_chi2_A = {p_chi2_A}')
@@ -80,28 +90,71 @@ def exercise_2_1():
 
   plt.show()
 
-def compute_B(v, T):
-  h = 6.626e-34
-  c = 299.7e6
-  kB = 1.381e-23
+def exercise_2_2(correlation):
+  v = 0.566e15
+  v_error = 0.025e15
 
-  first_term = (2 * h * v ** 3) / c ** 2
-  second_term = 1 / (math.exp( (h * v) / (kB * T) ) - 1)
+  T = 5.50e3
+  T_error = 0.29e3
 
-  return first_term * second_term
+  n_experiments = 1000
 
-v = 0.566e15
-T = 5.50e3
-print(f'v = {v}')
-print(f'T = {T}')
+  # Define parameters for two random numbers (Gaussianly distributed):
+  mu_v = v
+  sig_v =  v_error
+  mu_T = T
+  sig_T =  T_error
+  rho = correlation    # Correlation parameter
 
-B = compute_B(v, T)
-print(f'B = {B}')
+  # Method from class
+  theta = 0.5 * np.arctan(2.0 * rho * sig_v * sig_T / (np.square(sig_v) - np.square(sig_T)))
+  sigu = np.sqrt(np.abs((((sig_v*np.cos(theta)) ** 2) - (sig_T * np.sin(theta))**2 ) / ((np.cos(theta)) ** 2 - np.sin(theta)**2)))
+  sigv = np.sqrt(np.abs((((sig_T*np.cos(theta)) ** 2) - (sig_v * np.sin(theta))**2 ) / ((np.cos(theta)) ** 2 - np.sin(theta)**2)))
+
+  v = r.normal(0.0, sigu, n_experiments)
+  T = r.normal(0.0, sigv, n_experiments)
+  v_vals = mu_v + np.cos(theta) * v - np.sin(theta) * T
+  T_vals = mu_T + np.sin(theta) * v + np.cos(theta) * T
+  B_vals = np.zeros(len(v_vals))
+  # v_T_vals = np.array([v_vals, T_vals])
+
+  v_vals_std = np.std(v_vals)
+  T_vals_std = np.std(T_vals)
+
+  v_vals_mean = np.mean(v_vals)
+  T_vals_mean = np.mean(T_vals)
+
+
+  print(f'v_vals_mean = {v_vals_mean}')
+  print(f'v = {v}')
+
+  print(f'T_vals_mean = {T_vals_mean}')
+  print(f'T = {T}')
+
+
+  print(f'v_vals_std = {v_vals_std}')
+  print(f'v_error = {v_error}')
+
+
+  print(f'T_vals_std = {T_vals_std}')
+  print(f'T_error = {T_error}')
+
+
+  for i in range(len(B_vals)):
+    B_vals[i] = compute_B(v_vals[i], T_vals[i])
+
+  B_mean = np.mean(B_vals)
+  B_error = np.std(B_vals)
+
+  print(f'B_mean = {B_mean}')
+  print(f'B_error = {B_error}')
 
 
 def main():
   print('hello')
   # exercise_2_1()
+  # exercise_2_2(0)
+  # exercise_2_2(0.87)
 
 if __name__ == '__main__':
   main()
