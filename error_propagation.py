f6087fd0d649a8f6c4e53e264952a782b6e5abd2
Andrew <acsepreghy@ganintegrity.com>
2020-01-04 14:51:30 +0100

    removed unused imports

--- error_propagation.py
+++ error_propagation.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 from iminuit import Minuit
-import sys
 from scipy import stats
-from scipy.stats import binom, poisson, norm
-from scipy.integrate import quad, simps
-from scipy.optimize import minimize
 import math
 
 from distributions_probability import func_gaussian_pdf
